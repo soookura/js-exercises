20b463ad092ea63da9a52e49878bde2831580e64
So Kuramochi <So.Kuramochi@jp.ricoh.com>
2026-09-26 19:12:19 +0900

    コメント修正

--- ch05/ex09/index.js
+++ ch05/ex09/index.js
@@ -1,11 +1,3 @@
-// 任意の文字列を引数にとり、その文字列が JSON としてパース出来る場合
-// {success: true, data: <パースしたデータ>}を返し、
-// できない場合 {success: false, error: <エラー内容>} を返す関数を書きなさい
-
-// JSON.parse() メソッドを使用することで、
-// 上記の JSON 文字列を JavaScript オブジェクトに変換することができます。
-// 出題範囲: 5.5.7
-
 // メモ：https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
 // 解析する文字列が有効なJSONでない場合、SyntaxError
 
