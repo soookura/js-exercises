83af60f3ecb64cfa3479775768b56f605a1f853e
So Kuramochi <So.Kuramochi@jp.ricoh.com>
2026-09-26 18:41:02 +0900

    ch05/ex09テスト修正

--- ch05/ex09/index.test.js
+++ ch05/ex09/index.test.js
@@ -41,29 +41,22 @@ const jsonObject = {
 // 不正なJSONの例
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse#%E4%B8%8D%E6%AD%A3%E3%81%AA_json
 // から引用
-const notJsonText1 = "[1, 2, 3, 4, ]";
-const notJsonText2 = '{"foo": 1, }';
-const notJsonText3 = "{'foo': 1}";
-const notJsonText4 = "'string'";
+const notJsonTexts = [
+  ["不正例1", "[1, 2, 3, 4, ]"],
+  ["不正例2", '{"foo": 1, }'],
+  ["不正例3", "{'foo': 1}"],
+  ["不正例4", "'string'"],
+];
 
+// 以下テスト
+// 正常系
 test("正常な文字列で成功を返す", () => {
   expect(JsonParse(jsonText)).toEqual({ success: true, data: jsonObject });
 });
 
-test("不正な文字列で失敗を返す", () => {
-  expect(JsonParse(notJsonText1)).toEqual({
-    success: false,
-    error: expect.any(String),
-  });
-  expect(JsonParse(notJsonText2)).toEqual({
-    success: false,
-    error: expect.any(String),
-  });
-  expect(JsonParse(notJsonText3)).toEqual({
-    success: false,
-    error: expect.any(String),
-  });
-  expect(JsonParse(notJsonText4)).toEqual({
+// 異常系
+test.each(notJsonTexts)("%s", (name, notJsonText) => {
+  expect(JsonParse(notJsonText)).toEqual({
     success: false,
     error: expect.any(String),
   });
